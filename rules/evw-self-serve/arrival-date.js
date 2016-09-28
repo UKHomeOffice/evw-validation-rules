b35af0b0feb516080b32ec68ab34817030d1c863
Ralph Cowling <speak@ralph-cowling.com>
2016-09-28 17:55:01 +0100

    arrival-date now skips missing flightDetails
    - sometimes we won't have flight details, so won't be able to validate against them
    - This allows us to skip validation, rather than throwing a missing property error

--- rules/evw-self-serve/arrival-date.js
+++ rules/evw-self-serve/arrival-date.js
@@ -3,35 +3,37 @@
 const moment = require('moment');
 
 module.exports = (fieldValue, model) => {
-  let date = moment(`${fieldValue} ${model.get('flightDetails').arrivalTime}:00`, 'DD-MM-YYYY HH:mm:ss');
-  let threeMonths = moment().add(3, 'months');
-  let fourtyEight = moment().add(48, 'hours');
+  if(model.get('flightDetails')) {
+    let date = moment(`${fieldValue} ${model.get('flightDetails').arrivalTime}:00`, 'DD-MM-YYYY HH:mm:ss');
+    let threeMonths = moment().add(3, 'months');
+    let fourtyEight = moment().add(48, 'hours');
 
-  if (!date.isValid()) {
-    return {
-      length: {
-        minimum: 12,
-        message: 'arrival-date.invalid'
-      }
-    };
-  }
+    if (!date.isValid()) {
+      return {
+        length: {
+          minimum: 12,
+          message: 'arrival-date.invalid'
+        }
+      };
+    }
 
-  if (threeMonths.isBefore(date, 'second')) {
-    return {
-      length: {
-        minimum: 12,
-        message: 'arrival-date.too-far-in-future'
-      }
-    };
-  }
+    if (threeMonths.isBefore(date, 'second')) {
+      return {
+        length: {
+          minimum: 12,
+          message: 'arrival-date.too-far-in-future'
+        }
+      };
+    }
 
-  if (fourtyEight.isAfter(date, 'second')) {
-    return {
-      length: {
-        minimum: 12,
-        message: 'arrival-date.within-48-hours'
-      }
-    };
+    if (fourtyEight.isAfter(date, 'second')) {
+      return {
+        length: {
+          minimum: 12,
+          message: 'arrival-date.within-48-hours'
+        }
+      };
+    }
   }
 
   return false;
